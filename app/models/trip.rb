bdf3171d4d9601492853b1da2893ee6edf4f9597
Joe Jobes <jmrjobes@gmail.com>
2018-02-23 09:53:51 -0700

    Add routes and trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,15 @@
 class Trip < ApplicationRecord
   has_many :trails
 
+  def total_distance
+    trips.sum(:length)
+  end
 
+  def average_distance
+    trips.average(:length)
+  end
+
+  def longest_distance
+    trips.maximum(:length)
+  end
 end
